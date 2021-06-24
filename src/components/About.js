03c929f4a657070eaa1cabf8aee5b0ecb06b91a5
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-06-24 09:51:02 -0700

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function About( {bio, links} ) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length > 1 ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github = {Links.github} linkedin = {Links.linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
+// function HomeMaking(props) {
+//   return (<h1 style={{ color: props.color }}>
+//   {props.username} is a Web Developer from {props.city})
+//   </h1>);
+// }
+
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username = {user.name} city = {user.city} color = {user.color} />
+      <About bio = {user.bio} />
+      <Links github = {user.links.github} linkedin = {user.links.linkedin}/>
     </div>
   );
 }
